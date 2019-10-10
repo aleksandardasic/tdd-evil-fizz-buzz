856070c546bec7c0a7a11f04a6ff1ccb4254b445
Suzana Suvajac <suzana.suvajac@zuehlke.com>
2019-10-10 12:38:32 +0200

    Added Swap interface and FizzBuzz implementation

---
+++ fiz-buzz/FizzBuzzSwap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiz_buzz
+{
+    public class FizzBuzzSwap: ISwap
+    {
+        public IList<string> Swap(IList<string> list)
+        {
+            int number;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var num = list[i];
+                var success = Int32.TryParse(num, out number);
+                if (success && number % 3 == 0 && number % 5 == 0)
+                {
+                    list[i] = "FizzBuzz";
+                }
+            }
+
+            return list;
+        }
+    }
+}
