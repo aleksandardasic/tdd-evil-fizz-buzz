448b152459c5e01c6864e3ea4756894274eee76c
dutr <dusan.trpkovic@zuehlke.com>
2019-10-10 12:51:47 +0200

    output formatter

---
+++ fiz-buzz-tests/OutputFormatterTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using fiz_buzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fiz_buzz_tests
+{
+    [TestClass]
+    public class OutputFormatterTest
+    {
+        [TestMethod]
+        public void GivenAListOfStringsItFormatsItIntoString()
+        {
+            List<string> words = new List<string>();
+            words.Add("first");
+            words.Add("second");
+
+            OutputFormatter outputFormatter = new OutputFormatter();
+
+            Assert.AreEqual("first,second", outputFormatter.Format(words));
+        }
+    }
+}
---
+++ fiz-buzz/OutputFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiz_buzz
+{
+    public class OutputFormatter
+    {
+        public string Format(ICollection<string> words)
+        {
+            return string.Join(",", words);
+        }
+    }
+}
