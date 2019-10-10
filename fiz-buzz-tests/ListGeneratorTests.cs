314003182ad557f2dc4be949bc589acf44866e09
dutr <dusan.trpkovic@zuehlke.com>
2019-10-10 12:51:35 +0200

    list generator

---
+++ fiz-buzz-tests/ListGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fiz_buzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fiz_buzz_tests
+{
+    [TestClass]
+    public class ListGeneratorTests
+    {
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(50)]
+        [DataRow(99)]
+        public void NumberAtPositionIsCorrect(int position)
+        {
+            var listGenerator = new ListGenerator();
+            var generateList = listGenerator.GenerateList();
+            Assert.AreEqual(position+1, Int32.Parse(generateList[position]));
+        }
+    }
+}
---
+++ fiz-buzz/ListGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace fiz_buzz
+{
+    public class ListGenerator
+    {
+        public List<string> GenerateList()
+        {
+            var list = new List<string>(100);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                list.Add(i.ToString());
+            }
+
+            return list;
+        }
+    }
+}
